a3e4362da82e970b1cc83c2027d869c2f09ec652
abhishrent <abhishrent+github@gmail.com>
2025-09-11 13:05:22 +0545

    ready for merge

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+    for filename in os.listdir('./cogs'):
+        if filename.endswith('.py'):
+            cog_name = f'cogs.{filename[:-3]}'
+            try:
+                await bot.load_extension(cog_name)
+                print(f'Loaded cog: {cog_name}')
+            except Exception as e:
+                print(f'Failed to load cog {cog_name}: {e}')
